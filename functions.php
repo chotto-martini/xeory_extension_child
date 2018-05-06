147afef6c111252dae8d75acb60e6c73eab78659
chotto-martini <master@chotto-martini.net>
2018-05-06 12:57:46 +0900

    Revert "Revert "管理画面の投稿記事一覧に「アイキャッチ画像」を表示できるように修正。""
    This reverts commit e5a017edfdb9235177e3096916e77a79c4df472d.

--- functions.php
+++ functions.php
@@ -17,3 +17,31 @@ add_action('the_content', function ($content) {
 	$content = $content . get_content_blocks($post->ID);
 	return $content;
 });
+
+
+/** 投稿記事一覧にアイキャッチ画像を表示. */
+function customize_admin_manage_posts_columns($columns) {
+	$columns['thumbnail'] = __('Thumbnail');
+	return $columns;
+}
+function customize_admin_add_column($column_name, $post_id) {
+	if ( 'thumbnail' == $column_name) {
+		//テーマで設定されているサムネイルを利用する場合
+		//$thum = get_the_post_thumbnail($post_id, 'thumb100', array( 'style'=>'width:100px;height:auto;' ));
+		//Wordpressで設定されているサムネイル（小）を利用する場合
+		$thum = get_the_post_thumbnail($post_id, 'small', array( 'style'=>'width:100px;height:auto;' ));
+	}
+	if ( isset($thum) && $thum ) {
+		echo $thum;
+	}
+}
+/** アイキャッチ画像の列の幅をCSSで調整. */
+function customize_admin_css_list() {
+	echo '<style TYPE="text/css">.column-thumbnail{width:120px;}</style>';
+}
+/** カラムの挿入. */
+add_filter( 'manage_posts_columns', 'customize_admin_manage_posts_columns' );
+/** サムネイルの挿入. */
+add_action( 'manage_posts_custom_column', 'customize_admin_add_column', 10, 2 );
+/** 投稿一覧のカラムの幅のスタイル調整. */
+add_action('admin_print_styles', 'customize_admin_css_list', 21);
